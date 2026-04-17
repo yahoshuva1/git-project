f64af55edb720a205c86691ea8066f1b1389204e
yahoshuva1 <pyahoshuva@gmail.com>
2026-04-18 02:37:15 +0530

    added the sysout for required field

--- src/main/java/com/gitproject/DemoController.java
+++ src/main/java/com/gitproject/DemoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class DemoController {
 
+
     @GetMapping("/message")
     public String getMessage(){
         return "Hello learning git";
@@ -16,6 +17,7 @@ public class DemoController {
 
     @GetMapping("/send")
     public String sendMessage(){
+        System.out.println("Here is the print msg");
         return "Hi";
     }
 }
