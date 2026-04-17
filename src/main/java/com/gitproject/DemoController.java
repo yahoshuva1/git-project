fb7b9604254bec6d2b8783a715ad6765d600bab3
yahoshuva1 <pyahoshuva@gmail.com>
2026-04-18 02:08:05 +0530

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/gitproject/DemoController.java
@@ -0,0 +1,16 @@
+package com.gitproject;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class DemoController {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Hello learning git";
+    }
+}
